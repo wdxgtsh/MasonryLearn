620beec9a73914c428f0e5747a5d574ba3636603
zhaolei <zhaolei@allinmd.cn>
2016-04-18 23:13:51 +0800

    Initial Commit

---
+++ MasonryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryTest
+//
+//  Created by wdxgtsh on 16/4/18.
+//  Copyright © 2016年 wdxgtsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MasonryTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MasonryTest
+//
+//  Created by wdxgtsh on 16/4/18.
+//  Copyright © 2016年 wdxgtsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
